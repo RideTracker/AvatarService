296d4ca2400c4cf506d7069eb8a12f3b6d49e517
Nora Söderlund <norasoderlund@icloud.com>
2023-05-19 23:34:41 +0200

    Add Cloudflare Workers test

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import Jimp from "jimp";
+
+export default {
+    async fetch(request, env, ctx) {
+        const avatar = await Jimp.read("https://imagedelivery.net/iF-n-0zUOubWqw15Yx-oAg/6ad11bcb-b72f-4721-3247-4f4de4aa8f00/Avatar");
+
+        avatar.resize(100, 100).greyscale();
+
+        const buffer = avatar.getBufferAsync("image/png");
+
+        return new Response(buffer, {
+            headers: {
+                "Content-Type": "image/png"
+            }
+        });
+    }
+};
