a4c304ed2d99083fa5d5ff4f109fd63fd8290067
Nora Söderlund <norasoderlund@icloud.com>
2023-05-21 19:35:35 +0200

    Added endpoints

--- src/domains/router.ts
+++ src/domains/router.ts
@@ -1,13 +1,25 @@
-import { ThrowableRouter, withContent } from "itty-router-extras";
+import { ThrowableRouter, withContent, withParams } from "itty-router-extras";
 import { withAuth } from "../middlewares/auth";
 import { handleAvatarsRequest } from "../routes/avatars";
+import { handleCreateAvatarRequest } from "../routes/avatars/create";
+import { handleCreateAvatarColorRequest } from "../routes/avatars/colors/create";
+import { handleCreateAvatarImageRequest } from "../routes/avatars/images/create";
 
 export default function createRouter() {
     const router = ThrowableRouter();
 
     router.options("*", (request: any) => Response.json({ success: true }));
 
+    router.get("/api/ping", withContent, async (request: Request, env: Env) => {
+        return Response.json({
+            ping: "pong"
+        });
+    });
+
     router.get("/api/avatars", withAuth, withContent, handleAvatarsRequest);
+    router.post("/api/avatars", withAuth, withContent, handleCreateAvatarRequest);
+    router.post("/api/avatars/:avatarId/color", withAuth, withParams, withContent, handleCreateAvatarColorRequest);
+    router.post("/api/avatars/:avatarId/image", withAuth, withParams, withContent, handleCreateAvatarImageRequest);
 
     return router;
 };
