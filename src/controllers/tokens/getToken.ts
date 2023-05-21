5d3e5763df9180df8df517c7cb8b2727a49a0664
Nora Söderlund <norasoderlund@icloud.com>
2023-05-21 17:52:24 +0200

    Fixed query

--- src/controllers/tokens/getToken.ts
+++ src/controllers/tokens/getToken.ts
@@ -1,5 +1,5 @@
 import { Token } from "../../models/Token";
 
-export default async function getToken(database: D1Database, token: string): Promise<Token> {
-    return await database.prepare("SELECT * FROM tokens WHERE token = ?").bind(token).first();
+export default async function getToken(database: D1Database, key: string): Promise<Token> {
+    return await database.prepare("SELECT * FROM tokens WHERE key = ?").bind(key).first();
 };
