38ce07d351d980e69e2c42e501f95928d73f8a09
Nora Söderlund <nora.soderlund@polestar.com>
2023-05-23 13:30:54 +0200

    Added missing endpoint

--- src/domains/router.ts
+++ src/domains/router.ts
@@ -4,6 +4,7 @@ import { handleAvatarsRequest } from "../routes/avatars";
 import { handleCreateAvatarRequest } from "../routes/avatars/create";
 import { handleCreateAvatarColorRequest } from "../routes/avatars/colors/create";
 import { handleCreateAvatarImageRequest } from "../routes/avatars/images/create";
+import { handleRenderAvatarRequest } from "../routes/avatars/render";
 
 export default function createRouter() {
     const router = ThrowableRouter();
@@ -19,6 +20,7 @@ export default function createRouter() {
 
     router.get("/api/avatars", withAuth, withContent, handleAvatarsRequest);
     router.post("/api/avatars", withAuth, withContent, handleCreateAvatarRequest);
+    router.post("/api/avatars/render", withAuth, withContent, handleRenderAvatarRequest);
     router.post("/api/avatars/:avatarId/color", withAuth, withParams, withContent, handleCreateAvatarColorRequest);
     router.post("/api/avatars/:avatarId/image", withAuth, withParams, withContent, handleCreateAvatarImageRequest);
 
