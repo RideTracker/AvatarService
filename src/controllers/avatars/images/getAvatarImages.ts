f8148099956ad683f3233cf8563ef20641545d12
Nora Söderlund <nora.soderlund@polestar.com>
2023-07-03 18:26:09 +0200

    Fixed avatars query

--- src/controllers/avatars/images/getAvatarImages.ts
+++ src/controllers/avatars/images/getAvatarImages.ts
@@ -1,5 +1,5 @@
 import { AvatarImage } from "../../../models/AvatarImage";
 
 export async function getAvatarImages(database: D1Database): Promise<AvatarImage[] | null> {
-    return (await database.prepare("SELECT color_type AS colorType, avatar_images.* FROM avatar_images ORDER BY index ASC").all<AvatarImage>()).results ?? null;
+    return (await database.prepare(`SELECT color_type AS colorType, avatar_images.* FROM avatar_images ORDER BY "index" ASC`).all<AvatarImage>()).results ?? null;
 };
