936600e448e865701e26367694a25065ddbd9969
Nora Söderlund <nora.soderlund@polestar.com>
2023-07-05 21:54:28 +0200

    Added order to user avatars

--- src/controllers/users/getUserAvatars.ts
+++ src/controllers/users/getUserAvatars.ts
@@ -2,5 +2,5 @@ import { Avatar } from "../../models/Avatar";
 import { UserAvatar } from "../../models/UserAvatar";
 
 export async function getUserAvatars(database: D1Database, user: string): Promise<UserAvatar[] | null> {
-    return (await database.prepare("SELECT * FROM user_avatars WHERE user = ?").bind(user).all<UserAvatar>()).results ?? null;
+    return (await database.prepare("SELECT * FROM user_avatars WHERE user = ? ORDER BY timestamp DESC").bind(user).all<UserAvatar>()).results ?? null;
 };
